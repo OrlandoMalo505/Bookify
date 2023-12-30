1b51044ac073034b4092a5613fc029d72c502618
Orlando Malo <orlandomalo0@gmail.com>
2023-12-30 14:22:44 +0100

    Add project files.

---
+++ Bookify.Domain/Apartments/Amenity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Apartments;
+public enum Amenity
+{
+    WiFi = 1,
+    AirConditioning,
+    Parking,
+    PetFriendly,
+    SwimmingPool,
+    Gym,
+    Spa,
+    Terrace,
+    MountainView,
+    GardenView
+}
