3d584a72f5162aad8f447e44bb823e9759809e9f
Orlando Malo <orlandomalo0@gmail.com>
2023-12-30 16:32:37 +0100

    added repositories and unit of work

---
+++ Bookify.Domain/Users/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Users;
+public interface IUserRepository
+{
+    void Add(User user);
+
+    Task<User?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
